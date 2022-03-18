07d537b332d0a2a09f22b235934134a235fad880
Corentin Martin <corentin.martin@dynamicscreen.com>
2022-03-18 19:09:11 +0100

    fix timeout + duration custom

--- src/Slide/Slide.ts
+++ src/Slide/Slide.ts
@@ -7,14 +7,16 @@ import {
 } from "dynamicscreen-sdk-js"
 
 export default class Slide extends SlideModule {
+  private nextSlideTimeout: NodeJS.Timeout | null = null;
+
   async onReady() {
     await this.context.assetsStorage().then(async (ability: IAssetsStorageAbility) => {
-        if (this.context.slide.data.type === 'media') {
-          await ability.downloadAndGet(this.context.slide.data.media.url);
-        }
-      });
+      if (this.context.slide.data.type === 'media') {
+        await ability.downloadAndGet(this.context.slide.data.media.url);
+      }
+    });
 
-      return true
+    return true
   };
 
   setup(props: Record<string, any>, vue: VueInstance, context: ISlideContext) {
@@ -40,6 +42,25 @@ export default class Slide extends SlideModule {
         imageUrl.value = ability.getDisplayableAsset(this.context.slide.data.media.url).then((asset) => asset.displayableUrl());
       });
     });
+
+    this.context.onPlay(async () => {
+      if (this.context.slide.data?.duration) {
+        this.context.playbackManager.preventNextSlide(15);
+        this.nextSlideTimeout = setTimeout(() => {
+          this.context.playbackManager.next();
+        }, this.context.slide.data?.duration);
+      }
+    });
+
+    this.context.onReplay(async () => {
+      this.nextSlideTimeout?.refresh();
+    });
+
+    this.context.onEnded(async () => {
+      if (this.nextSlideTimeout) clearTimeout(this.nextSlideTimeout);
+    });
+
+
 
     return () => [
       h('div', { class: "flex justify-center items-center w-full h-full" }, [
